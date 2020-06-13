ff7c76908e5be82863c249a03867b1932a53a4e5
cloud1work1 <cloud1work1@gmail.com>
2020-06-13 15:58:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nwnyn3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nwnyn3)
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'WindStorm' 
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
